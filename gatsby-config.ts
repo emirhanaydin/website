92fed062c431ef81280c896f3e68f8a209bfeedd
emirhanaydin <emirhanaydin95@gmail.com>
2023-01-25 20:41:52 +0300

    install Contentful and implement on home page

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,27 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `website`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "Emirhan Aydın",
+    siteUrl: "https://www.emay.dev",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    "gatsby-plugin-image",
+  ],
+};
 
-export default config
+export default config;
